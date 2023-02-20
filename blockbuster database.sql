3cafc69e218291f614c230be6faef92413841dde
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-02-20 19:59:30 +0100

    Update blockbuster database.sql

--- blockbuster database.sql
+++ blockbuster database.sql
@@ -1,11 +1,31 @@
+1. use sakila database
+
 USE sakila;
+
+2. Get all the data from tables actor, film and customer.
+
 SELECT * FROM actor, film, customer;
+
+3. Get film titles.
 
 SELECT title from film;
 
+4. Get unique list of film languages under the alias language
+
 SELECT DISTINCT name FROM language;
+
+5. 
+
+a) Find out how many stores does the company have?
 
 SELECT COUNT(store_id)
 FROM store;
 
+b) Find out how many employees staff does the company have?
 
+SELECT COUNT(staff_id)
+FROM staff;
+
+c) Return a list of employee first names only?
+
+SELECT first_name FROM staff;
